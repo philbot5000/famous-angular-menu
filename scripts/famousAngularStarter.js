7d882803f9802f07c37f5a10f0a0731c0ae98e2c
philbot5000 <philbot5000@gmail.com>
2014-07-08 21:14:07 -0600

    Initial commit

---
+++ scripts/famousAngularStarter.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('famousAngularStarter',
+  ['ngAnimate', 'ngCookies',
+    'ngTouch', 'ngSanitize',
+    'ngResource', 'ui.router',
+    'famous.angular' ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'partials/main.html',
+        controller: 'MainCtrl'
+      })
+      .state('test', {
+        url: '/test',
+        templateUrl: 'partials/test.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
